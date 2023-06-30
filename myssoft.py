4c64ad60ebbb51e4bcf48e6f7631cb40fc3e7e23
sergio <mirchuk.sa02@gmail.com>
2023-06-30 16:06:22 +0200

    second_commit

--- myssoft.py
+++ myssoft.py
@@ -1,41 +1,68 @@
 from telethon.sync import TelegramClient
-from telethon import errors
-from telethon.tl import types
-import random
+from telethon.tl import functions
 import time
 
-# Константы, которые нужно заполнить
-api_id = '26969890'  # Идентификатор API
-api_hash = 'dae540e63757d45481eea42cdbdb2b42'  # Хэш API
-username = 'workinwarssaw'  # Имя пользователя аккаунта бабушки
+api_id = '28104407'  # Идентификатор API
+api_hash = '06f6924d26934caa4cfbb7508c837006'  # Хэш API
+code = ''
 
-def get_user_groups(api_id, api_hash):
-    with TelegramClient('sessаion_name22', api_id, api_hash) as client:
+message = 'Всем привет!'
+
+def get_group_participants(api_id, api_hash, code):
+    with TelegramClient('session_name', api_id, api_hash) as client:
+        client.sign_in(code=code)
+
         dialogs = client.get_dialogs(limit=None)
-        group_dialogs = [dialog for dialog in dialogs if isinstance(dialog.entity, types.Chat)]
-        return group_dialogs
+        group_dialogs = [dialog for dialog in dialogs if dialog.is_group]
 
-def send_message_with_delay(api_id, api_hash, message, delay=10):
-    with TelegramClient('session_name22', api_id, api_hash) as client:
-        group_dialogs = get_user_groups(api_id, api_hash)  # добавил получение списка групп здесь
-        for group_dialog in group_dialogs:
-            entity = group_dialog.entity
+        participants_list = []
+
+        for i, group_dialog in enumerate(group_dialogs, 1):
             try:
-                client.send_message(entity, message)
-                print(f"Сообщение отправлено в группу: {entity.title}")
-            except errors.rpcerrorlist.PeerFloodError:
-                print(f"Слишком много запросов. Пауза {delay} секунд.")
-                time.sleep(delay)
-            except errors.rpcerrorlist.ChatWriteForbiddenError:
-                print(f"Невозможно отправить сообщение в группу: {entity.title}. Пропускаем.")
+                participants = client.get_participants(group_dialog.id)
+                filtered_participants = {p.username for p in participants if p.username is not None}
+                if filtered_participants:
+                    participants_list.append({
+                        'group': group_dialog.title,
+                        'participants': filtered_participants
+                    })
             except Exception as e:
-                print(f"Ошибка при отправке сообщения в группу: {entity.title}. {e}")
-            else:
-                time.sleep(delay)
+                print(f"Ошибка при получении участников группы: {group_dialog.title}. {e}")
+            
+            progress = (i / len(group_dialogs)) * 100
+            print(f"Прогресс: {progress:.2f}%")
+        
+        # Сохранение списка участников в файл
+        with open('participants.txt', 'w') as file:
+            for group in participants_list:
+                file.write(f"Группа: {group['group']}\n")
+                file.write("Участники:\n")
+                for participant in group['participants']:
+                    file.write(f"@{participant}\n")
 
-def main():
-    message = 'Всем привет!'
-    send_message_with_delay(api_id, api_hash, message, delay=10)  # добавил параметр delay
+        print("Список уникальных участников сохранен в файл participants.txt")
 
-if __name__ == '__main__':
-    main()
+
+
+
+# def send_message_to_all_groups(api_id, api_hash, code, message):
+#     with TelegramClient('session_name', api_id, api_hash) as client:
+#         client.sign_in(code=code)
+
+#         dialogs = client.get_dialogs(limit=None)
+#         group_dialogs = [dialog for dialog in dialogs if dialog.is_group]
+
+#         for group_dialog in group_dialogs:
+#             try:
+#                 client(functions.messages.SendMessageRequest(
+#                     peer=group_dialog.id,
+#                     message=message
+#                 ))
+#                 print(f"Сообщение отправлено в группу: {group_dialog.title}")
+#             except Exception as e:
+#                 print(f"Ошибка при отправке сообщения в группу: {group_dialog.title}. {e}")
+
+#             time.sleep(5)  # Ожидание 5 секунд перед отправкой следующего сообщения
+
+# send_message_to_all_groups(api_id, api_hash, code, message)
+get_group_participants(api_id, api_hash, code)
--- new.py
+++ new.py
@@ -2,14 +2,45 @@ from telethon.sync import TelegramClient
 from telethon.tl import functions
 import time
 
-api_id = '26969890'  # Идентификатор API
-api_hash = 'dae540e63757d45481eea42cdbdb2b42'  # Хэш API
-code = '74321'
+api_id = '2810407'  # Идентификатор API
+api_hash = '06f6924d26934caa4cfbb7508c837006'  # Хэш API
+code = ''
 
 message = 'Всем привет!'
 
+def get_group_participants(api_id, api_hash, code):
+    with TelegramClient('session_namo', api_id, api_hash) as client:
+        client.sign_in(code=code)
+
+        dialogs = client.get_dialogs(limit=None)
+        group_dialogs = [dialog for dialog in dialogs if dialog.is_group]
+
+        participants_list = []
+
+        for group_dialog in group_dialogs:
+            try:
+                participants = client.get_participants(group_dialog.id)
+                participants_list.append({
+                    'group': group_dialog.title,
+                    'participants': participants
+                })
+            except Exception as e:
+                print(f"Ошибка при получении участников группы: {group_dialog.title}. {e}")
+
+    # Сохранение списка участников в файл
+    with open('participants.txt', 'w') as file:
+        for group in participants_list:
+            file.write(f"Группа: {group['group']}\n")
+            file.write("Участники:\n")
+            for participant in group['participants']:
+                file.write(f"- {participant.username}\n")
+
+    print("Список участников сохранен в файл participants.txt")
+
+
+
 def send_message_to_all_groups(api_id, api_hash, code, message):
-    with TelegramClient('session_name', api_id, api_hash) as client:
+    with TelegramClient('session_namo', api_id, api_hash) as client:
         client.sign_in(code=code)
 
         dialogs = client.get_dialogs(limit=None)
